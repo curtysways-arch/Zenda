0b052ca32594c3d5878ef3976cb48c681ae38e31
Carlos Caicedo <curtysways@gmail.com>
2026-09-03 00:15:51 -0500

    Add prisma generate to desplegar.bat

--- desplegar.bat
+++ desplegar.bat
@@ -11,7 +11,7 @@ git push origin main
 
 echo.
 echo 2. Conectando al VPS, compilando y reiniciando app...
-ssh root@citiox.com "cd /opt/Zenda || cd /root/zenda || cd /var/www/zenda ; git fetch origin main && git reset --hard origin/main ; sed -i 's#DATABASE_URL=.*#DATABASE_URL=\"file:./dev.db\"#g' .env ; export DATABASE_URL=\"file:./dev.db\" ; npx prisma db push ; npm run build ; pm2 restart all --update-env"
+ssh root@citiox.com "cd /opt/Zenda || cd /root/zenda || cd /var/www/zenda ; git fetch origin main && git reset --hard origin/main ; sed -i 's#DATABASE_URL=.*#DATABASE_URL=\"file:./dev.db\"#g' .env ; export DATABASE_URL=\"file:./dev.db\" ; npx prisma generate ; npx prisma db push ; npm run build ; pm2 restart all --update-env"
 
 echo.
 echo ===================================================
